19a65dc1c6b939c15c4225332e34fb2ccd60a956
Seiji Nakaya <snakaya@loosedays.jp>
2023-03-17 00:16:07 +0900

    chnge text decoration

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,9 @@
 export function Header() {
   return (
-    <div class="pt-12 pb-0 mx-auto">
+    <header class="pt-12 pb-0 mx-auto">
         <h1 class="flex justify-center">
-            <img class="max-w-screen-md w-8/12 md:w-4/12" src="/images/loosedays_logo.png" alt="LOOSEDAYS logo"/>
+            <img class="max-w-screen-md w-8/12 md:w-4/12 dark:hidden" src="/images/loosedays_logo_light.png" alt="LOOSEDAYS logo"/>
+            <img class="max-w-screen-md w-8/12 md:w-4/12 hidden dark:block" src="/images/loosedays_logo_dark.png" alt="LOOSEDAYS logo"/>
         </h1>
         <ul class="mx-auto max-w-screen-md py-10 flex flex-row justify-center text-center">
           <li class="flex-auto no-underline hover:underline"><a href="#top">TOP</a></li>
@@ -10,7 +11,11 @@ export function Header() {
           <li class="flex-auto no-underline hover:underline"><a href="#sec_company">COMPANY</a></li>
           <li class="flex-auto no-underline hover:underline"><a href="#sec_contact">CONTACT</a></li>
         </ul>
-        <h2 class="bg-cover bg-no-repeat bg-center" style="background: url(../images/mainImg.png)"><p class="py-40 text-5xl text-center text-gray-100">be smart loose, make your easy days.</p></h2>
-    </div>  
+        <h2 class="bg-cover bg-no-repeat bg-center" style="background: url(../images/mainImg.png)">
+          <p class="py-40 text-5xl text-center text-gray-100">
+            be smart <span class="" style="text-decoration-line: underline; text-underline-offset: 8px; text-decoration-style: dashed;">loose</span>, make your easy <span class="" style="text-decoration-line: underline; text-underline-offset: 8px; text-decoration-style: dashed;">days</span>.
+          </p>
+        </h2>
+    </header>  
   );
 }
